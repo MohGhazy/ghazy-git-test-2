659afd8f149aad3e334a4157d185b074a8ef8665
MohGhazy <mohghazyaa@gmail.com>
2024-11-10 20:35:05 +0700

    menambahkan paragraf dan list

--- index.php
+++ index.php
@@ -8,5 +8,10 @@
 <body>
  <h1>hello guys</h1>
  <h5>Lorem ipsum dolor sit amet.</h5>
+ <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, eum saepe at, accusamus aut expedita nihil quas veritatis quis incidunt ullam dolor iusto quos deserunt accusantium! Distinctio, totam et. Maiores consequatur, delectus cumque accusantium, ratione, ipsa voluptatum voluptates culpa quaerat sed error obcaecati reiciendis itaque consectetur quia omnis dolores. Incidunt.</p>
+ <ul>
+  <li>pertama</li>
+  <li>kedua</li>
+ </ul>
 </body>
 </html>
